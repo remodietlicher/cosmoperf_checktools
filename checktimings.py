9a9a4914f1031fef2dbd82c3fb215725421aa57e
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2015-09-22 15:47:49 +0200

    Improved timings parse script

--- checktimings.py
+++ checktimings.py
@@ -13,6 +13,7 @@ import sys
 sys.path.append("./tools") # this is the generic folder for subroutines
 sys.path.append('../checktools/tools')
 from ts_utilities import read_environ, dir_path
+from ts_fortran_nl import get_param
 
 # information
 __author__     = "Pascal Spoerri"
@@ -20,14 +21,44 @@ __copyright__  = "Copyright 2015, MeteoSwiss"
 __license__    = "GPL"
 __version__    = "1.0"
 
-def check():
+def parse():
     from cosmo_timings import COSMO_Run
     env = read_environ()
     rundir = env['RUNDIR']
     cosmolog = env['LOGFILE']
     slurmlog = env['LOGFILE_SLURM']
     name = "Cosmo run in "+rundir
-    data = COSMO_Run(folder=rundir, name=name, cosmolog=cosmolog, slurmlog=slurmlog)
+    return COSMO_Run(folder=rundir, name=name, cosmolog=cosmolog, slurmlog=slurmlog)
+
+def get_reference_timings():
+    import ConfigParser
+    config = ConfigParser.RawConfigParser()
+    timings_file = env['TIMINGS']
+    config.read(rundir+"/"+timings_file)
+
+    threshold = float(config.get('timings', 'threshold'))
+    timings = {}
+    for name, value in config.items("timings"):
+        if name is "threshold":
+            continue
+        timings[name] = float(value)
+    return (timings, threshold)
+
+def check_value(reference, value, threshold=0):
+    if value < (reference*(1.0+threshold/100.0)):
+        return True
+    return False
+
+def check():
+    data = parse()
     print(data)
+    timings, threshold = get_reference_timings()
+    status = 0
+    for name, timing in timings.iteritems():
+        data_timing = data[name]
+        if name in data and not check_value(timing, data_timing, threshold=threshold):
+            print("Fail: Could not validate "+name+": timing"+data_timing+"(reference timing: "+timing+")")
+            status = 40
+    return status
 if __name__ == "__main__":
     sys.exit(check())
