16001b197d6826719bbd0e2fa214b9def117113c
Guy de Morsier <guy.demorsier@meteoswiss.ch>
2017-05-15 15:18:23 +0200

    Put the tools and the yuspecif_owm_check.sh in git

--- yuspecif_owm_check.sh
+++ yuspecif_owm_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check alteration of the YUSPECIF file from OWM as compare to the benchmark
+# Check alteration of the YUSPECIF file from OWM as compared to the benchmark
 # Generate a warning and send mail if the diff between the YUSPECIF from owm
 # and the current YUSPECIF is different than a reference diff (check line count)
 #
@@ -18,21 +18,21 @@ status=$?
 # check if diff fail (missing file ...), status 1 is ok
 if [ $status -gt 1 ]; then
     echo "Error with diff command in yuspecif_owm_check.sh"
-    echo "diff ./YUSPECIF ./YUSPECIF_owm > tmp_diff_YUSPECIF_owm_vs_benchmark.txt"
+    echo "diff ./YUSPECIF ./owm_YUSPECIF > tmp_diff_YUSPECIF_owm_vs_benchmark.txt"
     echo `pwd`
     exit 1
 fi
 
 #compare length of diff
-nl_ref=`wc -l < diff_YUSPECIF_owm_vs_benchmark.txt`
+nl_ref=`wc -l < reference_${REAL_TYPE}/diff_YUSPECIF_owm_vs_benchmark.txt`
 nl_bench=`wc -l < tmp_diff_YUSPECIF_owm_vs_benchmark.txt`
 
 if [ "$nl_ref" -ne "$nl_bench" ]; then
-    echo "! Warning : New differences with YUSPECIF_owm:" |tee -a tmp_mail.txt
+    echo "! Warning : New differences with owm_YUSPECIF:" |tee -a tmp_mail.txt
     echo `pwd` |tee -a tmp_mail.txt
     echo "diff tmp_diff_YUSPECIF_owm_vs_benchmark.txt diff_YUSPECIF_owm_vs_benchmark.txt :"
-    diff tmp_diff_YUSPECIF_owm_vs_benchmark.txt diff_YUSPECIF_owm_vs_benchmark.txt |tee -a tmp_mail.txt
-    cat tmp_mail.txt | mail -s "COSMO - benchmark : changes against YUSPECIF_owm" "$recipients"
+    diff tmp_diff_YUSPECIF_owm_vs_benchmark.txt reference_${REAL_TYPE}/diff_YUSPECIF_owm_vs_benchmark.txt |tee -a tmp_mail.txt
+    cat tmp_mail.txt | mail -s "COSMO - benchmark : changes against owm_YUSPECIF" "$recipients"
     rm tmp_mail.txt
 fi
 rm tmp_diff_YUSPECIF_owm_vs_benchmark.txt
